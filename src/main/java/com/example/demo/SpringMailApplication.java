a2930792022d688654a204127826b84dcdedfc59
brahmareddy434 <brahmareddybachu5@gmail.com>
2023-03-14 14:10:15 +0530

    commit

---
+++ src/main/java/com/example/demo/SpringMailApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMailApplication.class, args);
+	}
+	
+
+}
